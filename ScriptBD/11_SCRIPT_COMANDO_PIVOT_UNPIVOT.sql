873bb9b0c3563e05a389320dbdfdb210e7f025fc
dbaassists <83104598+dbaassists@users.noreply.github.com>
2021-12-11 09:57:27 -0300

    Add files via upload

---
+++ ScriptBD/11_SCRIPT_COMANDO_PIVOT_UNPIVOT.sql
@@ -0,0 +1,100 @@
+-------------------------------------------------------------------------
+-- COMANDO UNPIVOT
+-------------------------------------------------------------------------
+
+
+SELECT  
+[NOME_LOJA]
+,[MES]
+,[VALOR]
+ 
+FROM
+(
+ 
+SELECT DISTINCT 
+ 
+[NOME_LOJA]
+, [JANEIRO]
+, [FEVEREIRO]
+, [MARCO]
+, [ABRIL]
+, [MAIO]
+, [JUNHO]
+, [JULHO]
+, [AGOSTO]
+, [SETEMBRO]
+, [OUTUBRO]
+, [NOVEMBRO]
+, [DEZEMBRO]
+ 
+FROM [dbo].[VENDAS_MES]
+ 
+) src
+ 
+UNPIVOT
+(
+[MES] FOR VALOR IN ([JANEIRO]
+, [FEVEREIRO]
+, [MARCO]
+, [ABRIL]
+, [MAIO]
+, [JUNHO]
+, [JULHO]
+, [AGOSTO]
+, [SETEMBRO]
+, [OUTUBRO]
+, [NOVEMBRO]
+, [DEZEMBRO])
+) upvt
+				
+
+-------------------------------------------------------------------------
+-- COMANDO PIVOT
+-------------------------------------------------------------------------
+
+
+SELECT  
+[NOME_LOJA]
+, [JANEIRO]
+, [FEVEREIRO]
+, [MARÇO]
+, [ABRIL]
+, [MAIO]
+, [JUNHO]
+, [JULHO]
+, [AGOSTO]
+, [SETEMBRO]
+, [OUTUBRO]
+, [NOVEMBRO]
+, [DEZEMBRO]
+ 
+FROM
+(
+ 
+SELECT DISTINCT 
+ 
+[NOME_LOJA]
+,[MES]
+,[VALOR]
+ 
+FROM [dbo].[VENDAS]
+ 
+) src
+ 
+PIVOT
+(
+SUM([VALOR]) FOR [MES] IN ([JANEIRO]
+, [FEVEREIRO]
+, [MARÇO]
+, [ABRIL]
+, [MAIO]
+, [JUNHO]
+, [JULHO]
+, [AGOSTO]
+, [SETEMBRO]
+, [OUTUBRO]
+, [NOVEMBRO]
+, [DEZEMBRO])
+) pvt
+				
+ORDER BY [NOME_LOJA]
